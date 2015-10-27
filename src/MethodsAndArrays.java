abb6b2345b9859addb225f74e167fce1cdbd94ba
carolgomesm <carolgomesm@gmail.com>
2015-10-27 16:13:31 -0400

    Added rollDice method

--- src/MethodsAndArrays.java
+++ src/MethodsAndArrays.java
@@ -1,13 +1,25 @@
+import java.security.SecureRandom;
 
 public class MethodsAndArrays {
 
 	public static void main(String[] args) {
-		long myLongVariable = 1000000L;
-		int myIntVariable;
 		
-		myIntVariable = (int)myLongVariable;
+		System.out.println(rollDice(2,6));
 		
 
+	}
+	
+	public static int rollDice(int numDice, int numSide){
+		
+		SecureRandom generateRandom = new SecureRandom();
+		int result = 0;
+		
+		for(int i = 0; i <= numDice - 1; i++ ){
+			
+			result += generateRandom.nextInt(numSide) + 1;
+		}
+		
+		return result;
 	}
 
 }
