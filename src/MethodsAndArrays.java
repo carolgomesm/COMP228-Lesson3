5c12ade1a96b040e728d46535a3565ac9522aa3a
carolgomesm <carolgomesm@gmail.com>
2015-10-27 11:27:02 -0400

    Added Driver Class

---
+++ src/MethodsAndArrays.java
@@ -0,0 +1,13 @@
+
+public class MethodsAndArrays {
+
+	public static void main(String[] args) {
+		long myLongVariable = 1000000L;
+		int myIntVariable;
+		
+		myIntVariable = (int)myLongVariable;
+		
+
+	}
+
+}
